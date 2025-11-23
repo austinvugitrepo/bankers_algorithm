b2b88867ec5431604111c832f16d9970fadf00e1
Austin <freeluck.v.i.p999@gmail.com>
2025-11-23 15:36:00 -0600

    added file checker functionality, suprisingly easier then i expected

--- banker.c
+++ banker.c
@@ -9,8 +9,18 @@ if(argc != 2){
   fprintf(stderr, "Too many arguments or none was provided! Only 1 file can be used for this program.\n");
   return 1;
 }
+//opening file
+FILE *f = fopen(argv[1], "r"); //opening file in read mode
+ 
+//error checking for if file does not exist
+if(f == NULL){
+    fprintf(stderr, "Can't open file.\n");
+    return 1;
+  }
 
-printf("Exact amount of arguments have been provided\n");
+printf("file exists!\n");
+
+fclose(f);
 
   return 0;
 }
